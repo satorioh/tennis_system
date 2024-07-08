15285394d6153053303e819071cb9f8570ec8d0d
Robin.Wang <3Jg$94sbQ@q1>
2024-07-08 17:47:57 +0800

    feat(project): add tennis ball train

--- yolo_infer.py
+++ yolo_infer.py
@@ -4,7 +4,9 @@ from ultralytics import YOLO, settings
 #                  'weights_dir': '/Users/robin/Git/tennis_system/weights',
 #                  'runs_dir': '/Users/robin/Git/tennis_system/runs'})
 print(settings)
+video_path = "./assert/input_video.mp4"
+image_path = "./assert/image.png"
 # Load an official or custom model
 model = YOLO("./model/yolov8x.pt")
 
-result = model.track("./assert/image.png", save=True)
+result = model.track(video_path, save=True)
