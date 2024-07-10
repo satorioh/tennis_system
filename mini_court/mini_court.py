4d7030157727e6135a74aff5e6f016c53eca0602
Robin.Wang <3Jg$94sbQ@q1>
2024-07-10 17:46:15 +0800

    feat(mini court): draw key points and lines

--- mini_court/mini_court.py
+++ mini_court/mini_court.py
@@ -123,11 +123,33 @@ class MiniCourt():
 
         return out
 
+    def draw_court(self, frame):
+        drawing_key_points_len = len(self.drawing_key_points)
+        for i in range(0, drawing_key_points_len, 2):
+            x = int(self.drawing_key_points[i])
+            y = int(self.drawing_key_points[i + 1])
+            cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
+
+        # draw Lines
+        for line in self.lines:
+            start_point = (int(self.drawing_key_points[line[0] * 2]), int(self.drawing_key_points[line[0] * 2 + 1]))
+            end_point = (int(self.drawing_key_points[line[1] * 2]), int(self.drawing_key_points[line[1] * 2 + 1]))
+            cv2.line(frame, start_point, end_point, (0, 0, 0), 2)
+
+        # Draw net
+        net_start_point = (
+            self.drawing_key_points[0], int((self.drawing_key_points[1] + self.drawing_key_points[5]) / 2))
+        net_end_point = (
+            self.drawing_key_points[2], int((self.drawing_key_points[1] + self.drawing_key_points[5]) / 2))
+        cv2.line(frame, net_start_point, net_end_point, (255, 0, 0), 2)
+
+        return frame
+
     def draw_mini_court(self, frames):
         print("Drawing mini court...")
         output_frames = []
         for frame in frames:
             frame = self.draw_background_rectangle(frame)
-            # frame = self.draw_court(frame)
+            frame = self.draw_court(frame)
             output_frames.append(frame)
         return output_frames
